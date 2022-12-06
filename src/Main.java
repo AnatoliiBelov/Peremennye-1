32a0fe6cff128a20e8fe0def74cfa7e57a93bc8b
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2022-12-06 16:21:35 +0300

    все задачи

--- src/Main.java
+++ src/Main.java
@@ -40,6 +40,35 @@ public class Main {
         System.out.println(frog);
         frog=frog+4;
         System.out.println(frog);
+        System.out.println();
+
+        var weightFighter1=78.2;
+        var weightFighter2=82.7;
+        var totalWeight=weightFighter1+weightFighter2;
+        System.out.println("Общий вес двух бойцов составляет "+totalWeight+" кг");
+        var differenceInWeight=weightFighter2-weightFighter1;
+        System.out.println("Разница в весе между 2мя бойцами составляет "+differenceInWeight+ " кг");
+        var differenceInWeight2= weightFighter2%weightFighter1;
+        System.out.println(differenceInWeight2);
+        System.out.println();
+
+        //Решите задачу с помощью функций сложения, вычитания, умножения и деления.
+        //
+        // 640 часов работы поделено между сотрудниками. Если каждый сотрудник посвящает работе 8 часов в день, то сколько всего работников в компании?
+        //Выведите результат задачи в консоль в формате "Всего работников в компании – … человек".
+        //
+        // Посчитайте сколько часов работы должно быть поделено между сотрудниками, если в компании работает на 94 человека больше. Выведите результат задачи в консоль в формате "Если в компании работает … человек, то всего … часов работы может быть поделено между сотрудниками".
+        //Для решения этой задачи используйте var
+
+        var totalWorkingHours=640;
+        var workingHoursOfOneEmployee=8;
+        var numberOfEmployees=totalWorkingHours/workingHoursOfOneEmployee;
+        System.out.println("Всего работников в компании "+numberOfEmployees+" человек");
+        numberOfEmployees=numberOfEmployees+94;
+        totalWorkingHours=numberOfEmployees*workingHoursOfOneEmployee;
+        System.out.println("Если в компании работает "+numberOfEmployees+" человек, то всего "+ totalWorkingHours+" часов работы может быть поделено между сотрудниками");
+
+
 
 
 
